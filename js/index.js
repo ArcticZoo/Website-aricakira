ab5f74ae6e29ad1205568b47896f1a0be14bc72a
ArcticZoo <a417803060@gmail.com>
2019-03-16 01:23:36 +0800

    3-16

---
+++ js/index.js
@@ -0,0 +1,101 @@
+
+	    (function ($){
+
+	      
+
+	      imagesLoaded(container, function () {
+	          wookmark = new Wookmark(container, {
+	          autoResize: true, // This will auto-update the layout when the browser window is resized.
+	          offset: 2, // Optional, the distance between grid items
+	          outerOffset: 0, // Optional, the distance to the containers border
+	          itemWidth: 385 // Optional, the width of a grid item
+	        });
+	      });
+
+          // 捕捉点击事件
+	      $('#container').on('click', 'li', function () {
+	        // Update the layout.
+	        wookmark.layout(true);
+	      });
+
+	      
+	      function onScroll() {
+	        // Check if we're within 100 pixels of the bottom edge of the broser window.
+	        var winHeight = window.innerHeight ? window.innerHeight : $window.height(), // iphone fix
+	            closeToBottom = ($window.scrollTop() + winHeight > $document.height() - 100);
+
+	        if (closeToBottom) {
+	          // Get the first then items from the grid, clone them, and add them to the bottom of the grid
+	          var $items = $('li', $container),
+	              $firstTen = $items.slice(0, 10).clone().css('opacity', 0);
+	          $container.append($firstTen);
+
+	          wookmark.initItems();
+	          wookmark.layout(true, function () {
+	            // Fade in items after layout
+	            setTimeout(function() {
+	              $firstTen.css('opacity', 1);
+	            }, 300);
+	          });
+	        }
+	      };
+
+	      // Capture scroll event.
+	      $window.bind('scroll.wookmark', onScroll);
+
+
+
+
+          //图片标签过滤功能
+		  // Setup filter buttons when jQuery is available
+	      var $filters = $('#filters li');
+
+	      /**
+	       * When a filter is clicked, toggle it's active state and refresh.
+	       */
+	      function onClickFilter(e) {
+	        var $item = $(e.currentTarget),
+	            activeFilters = [],
+	            filterType = $item.data('filter');
+
+	        if (filterType === 'all') {
+	          $filters.removeClass('active');
+	        } else {
+	          $item.toggleClass('active');
+
+	          // Collect active filter strings
+	          $filters.filter('.active').each(function() {
+	            activeFilters.push($(this).data('filter'));
+	          });
+	        }
+
+	        wookmark.filter(activeFilters, 'or');
+	      }
+
+	      // Capture filter click events.
+	      $('#filters').on('click.wookmark-filter', 'li', onClickFilter);
+
+
+	    })(jQuery);
+	  
+
+/*
+(function ($) {
+  var loadedImages = 0, // Counter for loaded images
+    handler = $('#tiles li'); // Get a reference to your grid items.
+
+
+  $('#tiles').imagesLoaded(function(){
+    // Call the layout function.
+    handler.wookmark(options);
+
+  }).progress(function(instance, image) {
+    // Update progress bar after each image load
+    loadedImages++;
+    if (loadedImages == handler.length)
+      $('.progress-bar').hide();
+    else
+      $('.progress-bar').width((loadedImages / handler.length * 100) + '%');
+  });
+})(jQuery);
+*/
